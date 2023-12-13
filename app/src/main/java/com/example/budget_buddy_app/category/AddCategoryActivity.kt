fb19a32b449341ab5119e4cc0b69dd5d7652b033
Melly <mebostjancic@edu.aau.at>
2023-12-13 21:33:05 +0100

    Added Additional Buttons and Navigation throughout several Activities

--- app/src/main/java/com/example/budget_buddy_app/category/AddCategoryActivity.kt
+++ app/src/main/java/com/example/budget_buddy_app/category/AddCategoryActivity.kt
@@ -1,12 +1,23 @@
 package com.example.budget_buddy_app.category
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.budget_buddy_app.MainActivity
 import com.example.budget_buddy_app.R
 
 class AddCategoryActivity : AppCompatActivity() {
+    lateinit var btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_category)
+        btn= findViewById(R.id.btn_save)
+
+        btn.setOnClickListener({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+
     }
 }
--- app/src/main/java/com/example/budget_buddy_app/inc_exp/EditInc_ExpActivity.kt
+++ app/src/main/java/com/example/budget_buddy_app/inc_exp/EditInc_ExpActivity.kt
@@ -1,12 +1,29 @@
 package com.example.budget_buddy_app.inc_exp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.budget_buddy_app.MainActivity
 import com.example.budget_buddy_app.R
 
 class EditInc_ExpActivity : AppCompatActivity() {
+    lateinit var btn: Button
+    lateinit var deleteBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.x_activity_edit_inc_exp)
+        setContentView(R.layout.activity_edit_inc_exp)
+
+        deleteBtn=findViewById(R.id.deleteBtn)
+
+        btn = findViewById(R.id.btn_save_edit)
+        btn.setOnClickListener({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+
+        btn.setOnClickListener({
+            finish()
+        })
     }
 }
